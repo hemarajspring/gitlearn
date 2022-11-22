7c7147febd57e53a51966e92cd0cae8f1f2002e5
hemarajspring <81808944+hemarajspring@users.noreply.github.com>
2022-11-22 11:28:06 +0530

    Create terra.tf

---
+++ terra.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+    description = "accesskey"  
+}
+variable "secret_key" {
+    description = "value"
+  
+}
+variable "account_id" {
+    description = "value223"
+  
+}
+variable "region" {
+    description = "enter geographic value"
+  
+}
